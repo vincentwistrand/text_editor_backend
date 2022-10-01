bb8228e3ead4039f87155ddc94b3baae315f55e0
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-10-02 00:09:59 +0200

    Login functions

---
+++ routes/auth.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+const usersModel = require('../models/users');
+
+
+router.post("/register", async (req, res) => {
+    const body = req.body;
+
+    console.log(body);
+
+    await usersModel.register(res, body);
+});
+
+router.post("/login", async (req, res) => {
+    const body = req.body;
+
+    console.log(body);
+
+    await usersModel.login(res, body);
+});
+
+router.get("/users", async (req, res) => {
+    const resultSet = await usersModel.getAllUsers()
+
+    res.json({data: resultSet});
+});
+
+router.delete("/user", async (req, res) => {
+    console.log(req.body)
+    const id = req.body.id;
+
+    await usersModel.deleteUser(id, res);
+})
+
+
+module.exports = router;
