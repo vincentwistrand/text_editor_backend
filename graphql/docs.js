fd5fe7308f8714a38bbce4ac6a73b4a6279c63de
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-10-16 12:39:35 +0200

    Add 'type' to object

--- graphql/docs.js
+++ graphql/docs.js
@@ -10,13 +10,13 @@ const DocType = new GraphQLObjectType({
     name: 'Document',
     description: 'This represents a document',
     fields: () => ({
-      _id: { type: GraphQLNonNull(GraphQLString) },
-      user: { type: GraphQLNonNull(GraphQLString) },
-      name: { type: GraphQLNonNull(GraphQLString) },
-      type: { type: GraphQLNonNull(GraphQLString) },
-      content: { type: GraphQLString },
-      access: { type: GraphQLList(GraphQLString) },
+        _id: { type: GraphQLNonNull(GraphQLString) },
+        user: { type: GraphQLNonNull(GraphQLString) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        type: { type: GraphQLNonNull(GraphQLString) },
+        content: { type: GraphQLString },
+        access: { type: GraphQLList(GraphQLString) },
     })
-})
+});
 
 module.exports = DocType;
