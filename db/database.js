c61f791ca7f94f5d63698c93bdefd62706b77e63
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-10-16 12:38:26 +0200

    Add local database for development

--- db/database.js
+++ db/database.js
@@ -1,12 +1,13 @@
 const mongo = require("mongodb").MongoClient;
-//const config = require("./config.json");
-//const collectionName = "docs";
-require('dotenv').config()
+
+require('dotenv').config();
 
 const database = {
-    getDb: async function getDb (collectionName = "docs") {
-        //let dsn = `mongodb+srv://myeditor:${process.env.ATLAS_PASSWORD}@cluster0.hvbargr.mongodb.net/editor?retryWrites=true&w=majority`;
-        let dsn = `mongodb://localhost:27017/editordev`;
+    getDb: async function getDb(collectionName = "docs") {
+        //let dsn = `mongodb+srv://myeditor:${process.env.ATLAS_PASSWORD}`+
+        //            `@cluster0.hvbargr.mongodb.net/editor?retryWrites=true&w=majority`;
+        let dsn = `mongodb://localhost:`+
+        `27017/editordev`;
 
         if (process.env.NODE_ENV === 'test') {
             dsn = `mongodb://localhost:27017/test`;
