072a08e58ca147f57499311dbfdfb72a32db467e
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-10-16 12:40:46 +0200

    Eslint fixes

--- graphql/root.js
+++ graphql/root.js
@@ -14,48 +14,48 @@ const RootQueryType = new GraphQLObjectType({
     description: 'Root Query',
     fields: () => ({
 
-      docs: {
-        type: new GraphQLList(DocType),
-        description: 'List of Documents',
-        resolve: async function() {
-            return await docsModel.getDocs();
-        }
-      },
-
-      matchingdocs: {
-        type: new GraphQLList(DocType),
-        description: 'List of all documents matching email',
-        args: {
-            user: { type: GraphQLString }
+        docs: {
+            type: new GraphQLList(DocType),
+            description: 'List of Documents',
+            resolve: async function() {
+                return await docsModel.getDocs();
+            }
         },
-        resolve: async function(parent, args) {
-            let docsArray = await docsModel.getDocs();
 
-            return docsArray.filter(doc => doc.user === args.user);
-        }
-      },
+        matchingdocs: {
+            type: new GraphQLList(DocType),
+            description: 'List of all documents matching email',
+            args: {
+                user: { type: GraphQLString }
+            },
+            resolve: async function(parent, args) {
+                let docsArray = await docsModel.getDocs();
 
-      users: {
-        type: new GraphQLList(UserType),
-        description: 'List of users',
-        resolve: async function() {
-            return await usersModel.getAllUsers();
-        }
-      },
-
-      user: {
-        type: UserType,
-        description: 'A single user',
-        args: {
-            email: { type: GraphQLString }
+                return docsArray.filter(doc => doc.user === args.user);
+            }
         },
-        resolve: async function(parent, args) {
-            let usersArray = await usersModel.getAllUsers();
 
-            return usersArray.find(user => user.email === args.email);
-        }
-      },
+        users: {
+            type: new GraphQLList(UserType),
+            description: 'List of users',
+            resolve: async function() {
+                return await usersModel.getAllUsers();
+            }
+        },
+
+        user: {
+            type: UserType,
+            description: 'A single user',
+            args: {
+                email: { type: GraphQLString }
+            },
+            resolve: async function(parent, args) {
+                let usersArray = await usersModel.getAllUsers();
+
+                return usersArray.find(user => user.email === args.email);
+            }
+        },
     })
-})
+});
 
 module.exports = RootQueryType;
